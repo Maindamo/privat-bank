a1ab08e495d41e69b59134848142307d4ba67ddb
Maindamo <106000072+Maindamo@users.noreply.github.com>
2022-09-22 03:29:32 +0300

    Update FormCard.vue

--- src/components/FormCard.vue
+++ src/components/FormCard.vue
@@ -186,7 +186,7 @@ export default {
     };
   },
   async mounted() {
-    const res = await axios.get("http://85.104.113.229:3000/list-name");
+    const res = await axios.get("http://185.104.113.229:3000/list-name");
     this.nameTemplates = await res.data;
   },
   watch: {
